00f77276e970863eccdc70e2d57b78749158e5ac
Muzaffar <aliyevmuhammadzafar@gmail.com>
2025-06-03 14:47:27 +0500

    commit too

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,16 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: false,
   devIndicators: false,
   compiler: {
     styledComponents: true,
   },
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(__dirname, '.');
+    return config;
+  }
 };
 
 export default nextConfig;
